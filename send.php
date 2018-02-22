736d89bef6df0ee220b13d257c5580f841829a3c
GordanaRajnjak <gogashkk@gmail.com>
2018-02-22 09:57:22 +0100

    Add files via upload

--- send.php
+++ send.php
@@ -40,7 +40,7 @@ try {
     $mail->AltBody = strip_tags($body);
 
     $mail->send();
-    echo 'Message has been sent';
+   // echo 'Message has been sent';
 } catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+  //  echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
