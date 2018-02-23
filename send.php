21c2c0306e4ceba2eb403bda66cb3e8bc27c5480
GordanaRajnjak <gogashkk@gmail.com>
2018-02-23 12:25:58 +0100

    Add files via upload

--- send.php
+++ send.php
@@ -17,7 +17,7 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
-   $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+   $mail->SMTPDebug = false;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
