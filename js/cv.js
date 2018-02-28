e593011f1835102ec8205e9aedd7cd165d3f4f61
GordanaRajnjak <gogashkk@gmail.com>
2018-02-28 09:55:58 +0100

    Add files via upload

--- js/cv.js
+++ js/cv.js
@@ -16,18 +16,4 @@ console.log(liid)
 
 // CAROUSEL SWIPE
 
-$(".carousel").on("touchstart", function(event){
-        var xClick = event.originalEvent.touches[0].pageX;
-    $(this).one("touchmove", function(event){
-        var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
-            $(this).carousel('prev');
-        }
-        else if( Math.floor(xClick - xMove) < -5 ){
-            $(this).carousel('next');
-        }
-    });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
-    });
-});
+$('.carousel').bcSwipe({ threshold: 50 });
